b29c0ae381c4de706c89a239e830fd1f4d9881f7
karthisiva <GriffinInfosystems1@mani-MacBook-Pro-2.local>
2015-03-24 21:23:34 +0530

    Initial Commit

---
+++ GoneVaca/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoneVaca
+//
+//  Created by Griffin on 24/03/15.
+//  Copyright (c) 2015 Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoneVaca/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoneVaca
+//
+//  Created by Griffin on 24/03/15.
+//  Copyright (c) 2015 Griffin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
